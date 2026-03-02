6b624a093a06e8352d3987f50433c56d9b3f2957
Saif Al-Islam <seif10seif@gmail.com>
2026-03-02 16:56:23 +0200

    Questione1

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -10,6 +10,11 @@ namespace assignment
     {
         static void Main(string[] args)
         {
+            #region Question1
+            //Q1 : What is the difference between static binding and dynamic binding? When does each one happen?
+            // Console.WriteLine("Static = decided before running the program.\r\nDynamic = decided while the program is running.");
+            #endregion
+
         }
     }
 }
